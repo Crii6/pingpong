81b647d88f4aef117f4c6d599a75b10cbfe0cb46
Crii6 <lecardonnelgm@gmail.com>
2023-06-21 17:28:04 +0200

    hover sur nav

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer>
+                                <div class="footerback"></div>
+                                <div class="pied-page">
+                                    <div class="logo-mornant">
+                                        <img src="img/logo-tt-white.png" alt="logo-tt-white">
+                                    </div>
+                                    <div class="footer-list">
+                                        <h3>Nous rejoindre</h3>
+                                        <ul>
+                                            <li><a href="">Entrainement</a></li>
+                                            <li><a href="">Contact</a></li>
+                                            <li><a href="">A propos</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="icon-footer">
+                                        <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+                                    </div>
+                                    <h3 class="footer-titre-partenaire">Nos partenaires</h3>
+                                    <div class="partenaire">
+                                        <a href="https://www.ville-mornant.fr/"><img src="img/mornantb.png" alt="logo-ville-de-mornant"></a>
+                                        <a href="fftt.com/site/accueil"><img src="img/fftt.png" alt="logo-fftt"></a>
+                                        <a href="http://www.castanosport.fr/"><img src="img/castano.png" alt="logo-castano"></a>
+                                        <a href="https://concessions.peugeot.fr/mornant"><img src="img/peugeot.png" alt="logo-peugeot"></a>
+                                        <a href="http://garage-morellon.fr/"><img src="img/citroen.png" alt="logo-citroen"></a>
+                                    </div>
+                                    
+                                    
+                                </div>
+                                
+                            </footer>
+                            <!-- <script src="test.js"></script> -->
+                            <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+                            <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+                            <script src="Js/jquery.js"></script>
+                            <script src="Js/plugin.js"></script>
+                            <script src="Js/main.js"></script>
+    
+</div>
+</body>
+
+</html>
